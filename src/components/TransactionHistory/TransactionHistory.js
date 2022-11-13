3efe1e793a3da81859045df5008ac5019cd1b78e
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-11-13 21:52:33 +0100

    finish task 4

---
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -0,0 +1,42 @@
+import React from "react";
+import css from "./TransactionHistory.module.css";
+import PropTypes from "prop-types";
+
+const TransactionHistory = ({ items }) => {
+	return (
+		<table className={css["transaction-history"]}>
+			<thead>
+				<tr>
+					<th>Type</th>
+					<th>Amount</th>
+					<th>Currency</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				{items.map((item) => {
+					return (
+						<tr key={item.id}>
+							<td>{item.type}</td>
+							<td>{item.amount}</td>
+							<td>{item.currency}</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
+};
+
+TransactionHistory.propTypes = {
+	item: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.string.isRequired,
+			type: PropTypes.string.isRequired,
+			amount: PropTypes.string.isRequired,
+			currency: PropTypes.string.isRequired,
+		})
+	),
+};
+
+export default TransactionHistory;
