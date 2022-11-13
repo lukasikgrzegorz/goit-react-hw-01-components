6c9548969f1d5c991922e9e95f8aef6245afe3de
lukasikgrzegorz <71289836+lukasikgrzegorz@users.noreply.github.com>
2022-11-13 20:38:23 +0100

    task 3 finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Profile from "./Profile/Profile";
 import Statistics from "./Statistics/Statistics";
+import FriendList from "./FriendList/FriendList";
 import user from "./../data/user.json";
 import data from "./../data/data.json";
+import friends from "./../data/friends.json";
 
 const App = () => {
 	return (
 		<>
 			<Profile user={user} />
 			<Statistics title="Upload stats" data={data} />
+			<FriendList friends={friends}/>
 		</>
 	);
 };
